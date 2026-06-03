bce00c3295424a414e1c13996b06c46cc894eabc
Muhammad Fadhil Adha <fadhil.sonic@gmail.com>
2026-06-04 00:36:05 +0700

    change navigation name

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -65,7 +65,7 @@
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                 d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
                         </svg>
-                        <span>Edit Questionnaire</span>
+                        <span>Survey Findings </span>
                     </a>
 
 
@@ -94,7 +94,7 @@
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                 d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                         </svg>
-                        <span>Questions</span>
+                        <span>Edit Questionnaire</span>
                     </a>
 
                     <div class="pt-4 mt-4 border-t border-gray-100 dark:border-gray-700">
